e3d8103158198ab1bedd8d1e4fae3160d593731d
Ash Charles <ashcharles@gmail.com>
2009-10-15 19:06:22 -0700

    another day, another dolar

--- robotrpcserver.cpp
+++ robotrpcserver.cpp
@@ -1,25 +1,45 @@
 #include "robotrpcserver.h"
 
+using namespace jsonrpc;
+
 //------------------------------------------------------------------------------
-RobotRpcServer::RobotRpcServer ( ARobot * robot, int port ) : mServer( port )
+RobotRpcServer::RobotRpcServer ( Rapi::ARobot * robot, int port )
+  : mServer ( port )
 {
   // try to get some basic devices
   mRobot = robot;
   mRobot->findDevice ( mDrivetrain, "drivetrain:0" );
   mRobot->findDevice ( mPowerPack, "powerpack:0" );
   mRobot->findDevice ( mRangeFinder, "ranger:0" );
-  mRobot->findDevice ( mLights, "lights:0" );
 
   // setup handlers as appropriate
-  if( mRangeFinder ) {
-    mServer.addMethodHandler( new jsonrpc::Server::RPCMethod< RobotRpcServer >
-                              (this, &RobotRpcServer::getRangeFinder ),
-                              "getRangeFinder" );
-    mServer.addMethodHandler( new jsonrpc::Server::RPCMethod< RobotRpcServer >
-                              (this, &RobotRpcServer::getRanges ), "getRanges" );
+  if ( mDrivetrain )
+  {
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getDrivetrainDev ),
+                               "getDrivetrainDev" );
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getDrivetrain ),
+                               "getDrivetrain" );
   }
-  // TODO: add other methods here
-
+  if ( mPowerPack )
+  {
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getPowerpackDev ),
+                               "getPowerpackDev" );
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getPowerpack ),
+                               "getPowerpack" );
+  }
+  if ( mRangeFinder )
+  {
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getRangeFinderDev ),
+                               "getRangeFinderDev" );
+    mServer.addMethodHandler ( new Server::RPCMethod< RobotRpcServer >
+                               ( this, &RobotRpcServer::getRanges ),
+                               "getRanges" );
+  }
 }
 //------------------------------------------------------------------------------
 RobotRpcServer::~RobotRpcServer()
@@ -28,45 +48,95 @@ RobotRpcServer::~RobotRpcServer()
 //------------------------------------------------------------------------------
 void RobotRpcServer::update()
 {
-  while( mServer.recv() );
+  while ( mServer.recv() );
 }
 //------------------------------------------------------------------------------
-void RobotRpcServer::getRanges( jsonrpc::variant params,
-                                jsonrpc::object& results,
-                                const std::string& ip,
-                                int port )
+variant packVelocity ( Rapi::CVelocity2d velocity )
 {
-  using namespace jsonrpc;
-  array ranges;
-  for( unsigned int i = 0; i < mRangeFinder->getNumSamples(); ++i ) {
-    ranges.push_back( toVariant<double>( mRangeFinder->mRangeData[i].range ) );
-  }
-  results[ "range" ] = toVariant( ranges );
+  object velocityObj;
+  velocityObj[ "xDot" ] = toVariant<double> ( velocity.mXDot );
+  velocityObj[ "yDot" ] = toVariant<double> ( velocity.mYDot );
+  velocityObj[ "yawDot" ] = toVariant<double> ( velocity.mYawDot );
+  return toVariant ( velocityObj );
 }
 //------------------------------------------------------------------------------
-void RobotRpcServer::getRangeFinder( jsonrpc::variant params,
-                                jsonrpc::object& results,
-                                const std::string& ip,
-                                int port )
+variant packPose ( Rapi::CPose2d pose )
 {
-  using namespace jsonrpc;
-
-  results[ "numSamples" ] = toVariant<int>( mRangeFinder->getNumSamples() );
-  double minRange = (mRangeFinder->getMinRange() == INFINITY ) ? 0.0 :
-                      mRangeFinder->getMinRange();
-  results[ "minRange" ] = toVariant<double>( minRange );
-  results[ "maxRange" ] = toVariant<double>( mRangeFinder->getMaxRange() );
-  results[ "beamConeAngle" ] = toVariant<double>( mRangeFinder->getBeamConeAngle() );
+  object poseObj;
+  poseObj[ "x" ] = toVariant<double> ( pose.mX );
+  poseObj[ "y" ] = toVariant<double> ( pose.mY );
+  poseObj[ "yaw" ] = toVariant<double> ( pose.mYaw );
+  return toVariant ( poseObj );
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getDrivetrainDev ( variant params,
+                                        object& results,
+                                        const std::string& ip,
+                                        int port )
+{
+  std::cout << "this method isn't needed" << std::endl;
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getPowerpackDev ( variant params,
+                                       object& results,
+                                       const std::string& ip,
+                                       int port )
+{
+  std::cout << "this method isn't needed" << std::endl;
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getRangeFinderDev ( variant params,
+        object& results,
+        const std::string& ip,
+        int port )
+{
+  results[ "numSamples" ] = toVariant<int> ( mRangeFinder->getNumSamples() );
+  double minRange = ( mRangeFinder->getMinRange() == INFINITY ) ? 0.0 :
+                    mRangeFinder->getMinRange();
+  results[ "minRange" ] = toVariant<double> ( minRange );
+  results[ "maxRange" ] = toVariant<double> ( mRangeFinder->getMaxRange() );
+  results[ "beamConeAngle" ] = toVariant<double> ( mRangeFinder->getBeamConeAngle() );
 
   array beamPose;
-  for( unsigned int i = 0; i < mRangeFinder->getNumSamples(); ++i ) {
-    Rapi::CPose2d pose( mRangeFinder->mRelativeBeamPose[i] );
-    object poseObj;
-    poseObj[ "x" ] = toVariant<double>( pose.mX );
-    poseObj[ "y" ] = toVariant<double>( pose.mY );
-    poseObj[ "yaw" ] = toVariant<double>( pose.mYaw );
-    beamPose.push_back( toVariant<object>( poseObj ) );
+  for ( unsigned int i = 0; i < mRangeFinder->getNumSamples(); ++i )
+  {
+    beamPose.push_back ( packPose ( mRangeFinder->mRelativeBeamPose[i] ) );
   }
-  results[ "beamPose" ] = toVariant<array>( beamPose );
+  results[ "beamPose" ] = toVariant<array> ( beamPose );
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getDrivetrain ( variant params,
+                                     object& results,
+                                     const std::string& ip,
+                                     int port )
+{
+  results[ "isStalled" ] = toVariant<bool> ( mDrivetrain->isStalled() );
+  results[ "stalledSince" ] = toVariant<double> ( mDrivetrain->stalledSince() );
+  results[ "measVelocity" ] = packVelocity ( mDrivetrain->getVelocity() );
+  results[ "cmdVelocity" ] = packVelocity ( mDrivetrain->getVelocityCmd() );
+  results[ "odometry" ] = packPose ( mDrivetrain->getOdometry()->getPose() );
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getPowerpack ( variant params,
+                                    object& results,
+                                    const std::string& ip,
+                                    int port )
+{
+  results[ "voltage" ] = toVariant<double> ( mPowerPack->getVoltage() );
+  results[ "remainingEnergy" ] = toVariant<double> ( mPowerPack->getBatteryLevel() );
+  results[ "isCharging" ] = toVariant<bool> ( mPowerPack->isCharging() );
+}
+//------------------------------------------------------------------------------
+void RobotRpcServer::getRanges ( variant params,
+                                 object& results,
+                                 const std::string& ip,
+                                 int port )
+{
+  array ranges;
+  for ( unsigned int i = 0; i < mRangeFinder->getNumSamples(); ++i )
+  {
+    ranges.push_back ( toVariant<double> ( mRangeFinder->mRangeData[i].range ) );
+  }
+  results[ "range" ] = toVariant ( ranges );
 }
 //------------------------------------------------------------------------------
--- robotrpcserver.h
+++ robotrpcserver.h
@@ -1,34 +1,72 @@
 #ifndef ROBOTRPCSERVER_H
 #define ROBOTRPCSERVER_H
 
-#include "jsonrpc_server.h"
 #include "RapiCore"
+#include "jsonrpc_server.h"
 
-using namespace Rapi;
 /**
   @author Ash Charles <jac27@sfu.ca>
 */
 class RobotRpcServer
 {
   public:
-    RobotRpcServer ( ARobot * robot, int port );
+    /** default constructor */
+    RobotRpcServer ( Rapi::ARobot * robot, int port );
+    /** default destructor */
     ~RobotRpcServer();
-    void update( void );
-    void getRanges( jsonrpc::variant params,
-                    jsonrpc::object& results,
-                    const std::string& ip,
-                    int port );
-    void getRangeFinder( jsonrpc::variant params,
+    /** listen for incoming requests (non-blocking) */
+    void update ( void );
+    //---- device configuration calls --------------------------------------------
+    /** sends the current drivetrain configuration */
+    void getDrivetrainDev ( jsonrpc::variant params,
+                            jsonrpc::object& results,
+                            const std::string& ip,
+                            int port );
+//     void getOdometryDev( jsonrpc::variant params,
+//                          jsonrpc::object& results,
+//                          const std::string& ip,
+//                          int port );
+    /** send the current powerpack configuration */
+    void getPowerpackDev ( jsonrpc::variant params,
+                           jsonrpc::object& results,
+                           const std::string& ip,
+                           int port );
+    /** send the current rangefinder configuration */
+    void getRangeFinderDev ( jsonrpc::variant params,
+                             jsonrpc::object& results,
+                             const std::string& ip,
+                             int port );
+    //---- device get/set calls --------------------------------------------------
+    /** send position and velocity information */
+    void getDrivetrain ( jsonrpc::variant params,
                          jsonrpc::object& results,
                          const std::string& ip,
                          int port );
+    /** send voltage and power information */
+    void getPowerpack ( jsonrpc::variant params,
+                        jsonrpc::object& results,
+                        const std::string& ip,
+                        int port );
+    /** send a rangefinder data */
+    void getRanges ( jsonrpc::variant params,
+                     jsonrpc::object& results,
+                     const std::string& ip,
+                     int port );
   private:
+    /** utility routine to pack a CVelocity2d object into a jsonrpc::variant */
+    jsonrpc::variant packVelocity ( Rapi::CVelocity2d velocity );
+    /** utility routine to pack a CPose2d object into a jsonrpc::variant */
+    jsonrpc::variant packPose ( Rapi::CPose2d pose );
+    /** json server */
     jsonrpc::TCPServer mServer;
-    ARobot * mRobot;
-    ADrivetrain2dof * mDrivetrain;
-    APowerPack * mPowerPack;
-    ARangeFinder * mRangeFinder;
-    ALights * mLights;
+    /** a pointer to the robot object we are serving */
+    Rapi::ARobot * mRobot;
+    /** the drivetrain object we are serving (if it exists) */
+    Rapi::ADrivetrain2dof * mDrivetrain;
+    /** the powerpack object we are serving (if it exists) */
+    Rapi::APowerPack * mPowerPack;
+    /** the rangefinder object we are serving (if it exists) */
+    Rapi::ARangeFinder * mRangeFinder;
 };
 
 #endif
