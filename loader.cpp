4e361e9bb7e3f4245cf0cae015eb73bea1dfd2e6
Ash Charles <ash@gumstix.com>
2010-09-10 22:59:09 -0700

    Clean up the error handling for ltdl errors with a goto.
    Using a goto means our variables have to be initialized prior to any
    jumps even if we are not using them---we have two options:
    1. Initialize the variables at the start. This seems cleaner for now.
    2. Wrap the main body in braces to limit the scope of these automatic
       variables.  This may be better in the long run even if it looks
       a little gross right now.

--- loader.cpp
+++ loader.cpp
@@ -10,25 +10,22 @@
 
 int main()
 {
-	int errors = lt_dlinit();
-	if (errors) {
-		printf("Libtool error: %s\nQuitting...\n", lt_dlerror());
-		exit(-1);
-	}
-	lt_dlhandle handle = lt_dlopenext( "transport" );
-	if ( ! handle ) {
-		printf("Libtool error: %s\nQuitting...\n", lt_dlerror());
-		exit(-1);
-	}
-	void * func = lt_dlsym( handle, "Init" );
-	if (! handle ) {
-		printf("Libtool error: %s\nQuitting...\n", lt_dlerror());
-		exit(-1);
-	}
+	lt_dlhandle handle;
+	void * func;
+
+	if ( lt_dlinit() != 0 ) { goto ltdl_fail; }
+	handle = lt_dlopenext( "transport" );
+	if ( ! handle ) { goto ltdl_fail; }
+	func = lt_dlsym( handle, "Init" );
+	if (! func ) { goto ltdl_fail; }
 	printf("Successfully opened!\n");
-	if ( lt_dlexit() != 0 ) {
-		printf("Libtool error: %s\nQuitting...\n", lt_dlerror());
-		exit(-1);
-	}
+	if ( lt_dlexit() != 0 ) { goto ltdl_fail; }
 	return 0;
+
+// GOTO label to handle ltdl error. An irritating construct but we don't
+// have exceptions available in ltdl.
+ltdl_fail:
+    printf("Libtool error: %s\nQuitting...\n", lt_dlerror());
+    lt_dlexit(); // okay if it fails...we're just trying to clean up.
+    return -1;
 }
